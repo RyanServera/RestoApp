26463502a9ae84036ec3850c3a359d3cc89519bc
alliMD <allison.mejiadiestra@mail.mcgill.ca>
2018-02-02 12:49:30 -0500

    New Model ver. 2.0 and blank controller and persistence

---
+++ RestoApp/src/ca/mcgill/ecse223/resto/controller/Controller.java
@@ -0,0 +1,36 @@
+package ca.mcgill.ecse223.resto.controller;
+
+public class Controller {
+	
+	/**
+	 * Controller constructor 
+	 */
+	public Controller(){
+		
+	}
+	
+	/*
+	 * Feature 1: Add tables and seats 
+	 * authors: Bill 
+	 */
+	
+	/*
+	 * Feature 2: Remove tables
+	 * authors: Jake
+	 */
+	
+	/*
+	 * Feature 3: Update table number and number of seats
+	 * authors: Allison
+	 */
+	
+	/*
+	 * Feature 4: Change location
+	 * authors: Thomas Labourdette
+	 */
+	
+	/*
+	 * Feature 5: Display Menu
+	 * authors: Ryan, Jonathan
+	 */
+}
