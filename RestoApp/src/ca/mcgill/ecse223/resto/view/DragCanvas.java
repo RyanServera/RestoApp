f2352a3705f2d601cdd4a2592c6937aa26a308a4
alliMD <allison.mejiadiestra@mail.mcgill.ca>
2018-03-23 11:30:16 -0400

    new constructor for GCanvas that adds existing tables to the Canvas when app starts

--- RestoApp/src/ca/mcgill/ecse223/resto/view/DragCanvas.java
+++ RestoApp/src/ca/mcgill/ecse223/resto/view/DragCanvas.java
@@ -4,8 +4,11 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
+import java.util.List;
 
 import acm.graphics.GCanvas;
+import ca.mcgill.ecse223.resto.model.Seat;
+import ca.mcgill.ecse223.resto.model.Table;
 
 public class DragCanvas extends GCanvas implements MouseListener, MouseMotionListener{
 	public static ArrayList<GLabeledRect> labeledRects;
@@ -15,10 +18,7 @@ public class DragCanvas extends GCanvas implements MouseListener, MouseMotionLis
 	int tabNum;
 	GLabeledRect rect;
 	
-	public DragCanvas(){
-				
-	}
-	public DragCanvas (double xTable, double yTable, double widthTable, double lengthTable, int tableNum) {
+	public DragCanvas(double xTable, double yTable, double widthTable, double lengthTable, int tableNum){
 		x = xTable;
 		y = yTable;
 		width = widthTable;
@@ -30,10 +30,34 @@ public class DragCanvas extends GCanvas implements MouseListener, MouseMotionLis
 		
 		addMouseListener(this);
 		addMouseMotionListener(this);
+	}
+	public DragCanvas(List<Table> list) {
+		labeledRects =  new ArrayList<GLabeledRect>();
+		for(Table table: list){
+			System.out.println("In DragCanvas, table values, table num = "+ table.getNumber()+" x = "+table.getX()+", y = "+table.getY()+" ");
+			rect = new GLabeledRect(table.getX(), table.getY(), table.getLength(), table.getWidth(), table.getNumber());
+			labeledRects.add(rect);	
+			add(rect);
+		}
+	}
+	
+	public void addTableToCanvas (double xTable, double yTable, double widthTable, double lengthTable, int tableNum) {
+		x = xTable;
+		y = yTable;
+		width = widthTable;
+		height = lengthTable;
+		tabNum = tableNum;
+		
+		rect = new GLabeledRect(x, y, width, height, tableNum);
+		labeledRects.add(rect);	
+		add(rect);
+		
+		addMouseListener(this);
+		addMouseMotionListener(this);
 				
 	}
 	
-	public DragCanvas (GLabeledRect rect){
+	public void addGRectToCanvas (GLabeledRect rect){
 		labeledRects.add(rect);
 		add(rect);
 	}
