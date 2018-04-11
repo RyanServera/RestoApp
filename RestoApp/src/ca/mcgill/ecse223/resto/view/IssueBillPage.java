b1aa6c4f27eecafd478b28f474dc576516ae42e9
jacob-hochstrasser <jacob.hochstrasser@mail.mcgill.ca>
2018-04-11 10:43:17 -0400

    Updated issueBillPage to reflect new controller method. Moved coupon ui to issueBillPage

--- RestoApp/src/ca/mcgill/ecse223/resto/view/IssueBillPage.java
+++ RestoApp/src/ca/mcgill/ecse223/resto/view/IssueBillPage.java
@@ -3,13 +3,16 @@ package ca.mcgill.ecse223.resto.view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
@@ -23,38 +26,44 @@ import ca.mcgill.ecse223.resto.model.Seat;
 import ca.mcgill.ecse223.resto.model.Table;
 
 public class IssueBillPage extends JFrame{
-	
+	//TODO refactor ui to match new controller implementation
 	//private static final long serialVersionUID = 2L;
-
+	
 	//UI elements
 	private JLabel errorMessage;
-	private JLabel currentOrdersLabel;
+	
 	private JLabel tablesLabel;
-	private JLabel currentSeatsLabel;
-	private JLabel billsLabel;
+	private JLabel seatsLabel;
+	private JLabel percentageLabel; 
 	
-	private JButton issueBill;
-	private JButton addToBill;
+	private JButton addSeatToListButton;
+	private JButton issueBillButton;
+	private JButton generateCouponButton; 
 	
-	private JComboBox<String> currentOrdersComboBox;
 	private JComboBox<String> tablesComboBox;
-	private JComboBox<String> currentSeatsComboBox;
-	private JComboBox<String> billsComboBox;
+	private JComboBox<String> seatsComboBox;
+	private JTextField percentageInput;
 	
 	//data for selecting an order from our list of current orders
-	private Integer selectedOrder = -1;
-	private HashMap<Integer, Order> currentOrders;
-	
 	private Integer selectedTable = -1;
 	private HashMap<Integer, Table> tables;
-	
 	private Integer selectedSeat = -1;
-	private HashMap<Integer, Seat> currentSeats;
+	private HashMap<Integer, Seat> seats;
 	
-	private Integer selectedBill = -1;
-	private HashMap<Integer, Bill> bills;
+	private List<Seat> currentSeats;
 
 	private String error = null;
+	
+	//obsolete elements
+	//private JLabel currentOrdersLabel;
+	//private JButton addToBill;
+	//private JLabel billsLabel;
+	//private JComboBox<String> currentOrdersComboBox
+	//private JComboBox<String> billsComboBox;
+	//private Integer selectedOrder = -1;
+	//private HashMap<Integer, Order> currentOrders;
+	//private Integer selectedBill = -1;
+	//private HashMap<Integer, Bill> bills;
 	
 	public IssueBillPage() {
 		initComponents();
@@ -69,18 +78,34 @@ public class IssueBillPage extends JFrame{
 		errorMessage = new JLabel();
 		errorMessage.setForeground(Color.RED);
 		
-		currentOrdersLabel = new JLabel("Order: ");
 		tablesLabel = new JLabel("Table: ");
-		currentSeatsLabel = new JLabel("Seat: ");
-		billsLabel = new JLabel("Bill: ");
+		seatsLabel = new JLabel("Seat: ");
+		percentageLabel = new JLabel("Discount Percentage");
 		
+		percentageInput = new JTextField(); 
+		percentageInput.setText("%");
+		percentageInput.setColumns(10);
 		
-		currentOrdersComboBox = new JComboBox<String>(new String[0]);
-		currentOrdersComboBox.addActionListener(new java.awt.event.ActionListener() {
+		currentSeats = new ArrayList<Seat>();
+		
+		addSeatToListButton = new JButton("Add Seat");
+		addSeatToListButton.addActionListener(new java.awt.event.ActionListener(){
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				JComboBox<String> cb = (JComboBox<String>) evt.getSource();
-				selectedOrder = cb.getSelectedIndex();
-				updateLists();
+				addSeatToListButtonActionPerformed(evt);
+			}
+		});
+		
+		issueBillButton = new JButton("Issue Bill");
+		issueBillButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				issueBillButtonActionPerformed(evt);
+			}
+		});
+		
+		generateCouponButton = new JButton("Generate Coupon");
+		generateCouponButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				generateCouponButtonActionPerformed(evt);
 			}
 		});
 		
@@ -93,33 +118,11 @@ public class IssueBillPage extends JFrame{
 			}
 		});
 		
-		currentSeatsComboBox = new JComboBox<String>(new String[0]);
-		currentSeatsComboBox.addActionListener(new java.awt.event.ActionListener() {
+		seatsComboBox = new JComboBox<String>(new String[0]);
+		seatsComboBox.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				JComboBox<String> cb = (JComboBox<String>) evt.getSource();
 				selectedSeat = cb.getSelectedIndex();
-			}
-		});
-		
-		billsComboBox = new JComboBox<String>(new String[0]);
-		billsComboBox.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				JComboBox<String> cb = (JComboBox<String>) evt.getSource();
-				selectedBill = cb.getSelectedIndex();
-			}
-		});
-		
-		issueBill = new JButton("Issue New Bill");
-		issueBill.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				issueBillButtonActionPerformed(evt);
-			}
-		});
-		
-		addToBill = new JButton("Add Seat to Bill");
-		addToBill.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				addToBillButtonActionPerformed(evt);
 			}
 		});
 		
@@ -130,128 +133,186 @@ public class IssueBillPage extends JFrame{
 		layout.setHorizontalGroup(
 				layout.createParallelGroup()
 					.addGroup(layout.createSequentialGroup()
-						.addComponent(currentOrdersLabel)
-						.addComponent(currentOrdersComboBox))
-					.addGroup(layout.createSequentialGroup()
 						.addComponent(tablesLabel)
 						.addComponent(tablesComboBox))
 					.addGroup(layout.createSequentialGroup()
-						.addComponent(currentSeatsLabel)
-						.addComponent(currentSeatsComboBox))
+						.addComponent(seatsLabel)
+						.addComponent(seatsComboBox))
 					.addGroup(layout.createSequentialGroup()
-						.addComponent(billsLabel)
-						.addComponent(billsComboBox))
+						.addComponent(addSeatToListButton)
+						.addComponent(issueBillButton))
+					.addGroup(layout.createSequentialGroup()
+						.addComponent(percentageLabel)
+						.addComponent(percentageInput))
+					.addGroup(layout.createSequentialGroup()
+						.addComponent(generateCouponButton))
+					
 				);
-		layout.linkSize(SwingConstants.VERTICAL, new java.awt.Component[] {currentOrdersLabel, currentOrdersComboBox});
-		layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] { currentOrdersLabel, currentOrdersComboBox});
 		
-		layout.linkSize(SwingConstants.VERTICAL, new java.awt.Component[] {tablesLabel, tablesComboBox});
-		layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] { tablesLabel, tablesComboBox});
-		
-		layout.linkSize(SwingConstants.VERTICAL, new java.awt.Component[] {currentSeatsLabel, currentSeatsComboBox});
-		layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] { currentSeatsLabel, currentSeatsComboBox});
-		
-		layout.linkSize(SwingConstants.VERTICAL, new java.awt.Component[] {billsLabel, billsComboBox});
-		layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] { billsLabel, billsComboBox});
+		layout.linkSize(SwingConstants.VERTICAL, new java.awt.Component[] {tablesLabel, tablesComboBox, seatsLabel, seatsComboBox, percentageLabel, percentageInput, generateCouponButton});
+		layout.linkSize(SwingConstants.HORIZONTAL, new java.awt.Component[] {tablesLabel, tablesComboBox, seatsLabel, seatsComboBox, percentageLabel, percentageInput, generateCouponButton});
 		
 		layout.setVerticalGroup(
 				layout.createSequentialGroup()
 					.addGroup(layout.createParallelGroup()
-						.addComponent(currentOrdersLabel)
-						.addComponent(currentOrdersComboBox))
-					.addGroup(layout.createParallelGroup()
 						.addComponent(tablesLabel)
 						.addComponent(tablesComboBox))
 					.addGroup(layout.createParallelGroup()
-						.addComponent(currentSeatsLabel)
-						.addComponent(currentSeatsComboBox))
+						.addComponent(seatsLabel)
+						.addComponent(seatsComboBox))
 					.addGroup(layout.createParallelGroup()
-						.addComponent(billsLabel)
-						.addComponent(billsComboBox))
+						.addComponent(addSeatToListButton)
+						.addComponent(issueBillButton))
+					.addGroup(layout.createParallelGroup()
+						.addComponent(percentageLabel)
+						.addComponent(percentageInput))
+					.addGroup(layout.createParallelGroup()
+						.addComponent(generateCouponButton))
 				);
 		
 		pack();
 		setVisible(true);
+		
+		//currentOrdersLabel = new JLabel("Order: ");
+		//billsLabel = new JLabel("Bill: ");
+		/*currentOrdersComboBox = new JComboBox<String>(new String[0]);
+		currentOrdersComboBox.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				JComboBox<String> cb = (JComboBox<String>) evt.getSource();
+				selectedOrder = cb.getSelectedIndex();
+				updateLists();
+			}
+		});*/
+		
+		/*billsComboBox = new JComboBox<String>(new String[0]);
+		billsComboBox.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				JComboBox<String> cb = (JComboBox<String>) evt.getSource();
+				selectedBill = cb.getSelectedIndex();
+			}
+		});*/
+		
+		/*addToBill = new JButton("Add Seat to Bill");
+		addToBill.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				addToBillButtonActionPerformed(evt);
+			}
+		});*/
 	}
 	
 	private void refreshData() {
 		errorMessage.setText(error);
 		if(error == null || error.length() == 0) {
-			currentOrders = new HashMap<Integer, Order>();
-			currentOrdersComboBox.removeAllItems();
 			tables = new HashMap<Integer, Table>();
 			tablesComboBox.removeAllItems();
-			currentSeats = new HashMap<Integer, Seat>();
-			currentSeatsComboBox.removeAllItems();
-			bills = new HashMap<Integer, Bill>();
-			billsComboBox.removeAllItems();
 			Integer index = 0;
-			for(Order o2 : Controller.listAllCurrentOrders()) {
-				currentOrders.put(index, o2);
-				currentOrdersComboBox.addItem("Order # " + o2.getNumber());
+			for (Table t : Controller.listAllTables()) {
+				tables.put(index, t);
+				tablesComboBox.addItem("Table #" + t.getNumber());
 				index++;
-			};
+			}
+			selectedTable = -1;
+			currentSeats.clear();
+			seats.clear();
+			seatsComboBox.removeAllItems();
+			selectedSeat = -1;
 		}
 		pack();
+		
+		//currentOrders = new HashMap<Integer, Order>();
+		//currentOrdersComboBox.removeAllItems();
+		//bills = new HashMap<Integer, Bill>();
+		//billsComboBox.removeAllItems();
+		//Integer index = 0;
+		//for(Order o2 : Controller.listAllCurrentOrders()) {
+		//	currentOrders.put(index, o2);
+		//	currentOrdersComboBox.addItem("Order # " + o2.getNumber());
+		//	index++;
+		//};
 	}
 	
 	private void updateLists() {
 		errorMessage.setText(error);
 		if(error == null || error.length() == 0) {
-			RestoApp ra = RestoApplication.getRestoApp();
-			Order o = ra.getCurrentOrder(selectedOrder);
-			Table t = o.getTable(selectedTable);
-			Seat s = t.getCurrentSeat(selectedSeat);
-			//Bill b = s.getBill(selectedBill);
+			Table t = tables.get(selectedTable);
+			seats = new HashMap<Integer,Seat>();
+			seatsComboBox.removeAllItems();
 			Integer index = 0;
-			for(Order o2 : Controller.listAllCurrentOrders()) {
+			for(Seat s : t.getSeats()) {
+				seats.put(index, s);
+				seatsComboBox.addItem("Seat # " + (t.indexOfCurrentSeat(s)+1));
+				index++;
+			}
+			//RestoApp ra = RestoApplication.getRestoApp();
+			//Order o = ra.getCurrentOrder(selectedOrder);
+			//Seat s = t.getCurrentSeat(selectedSeat);
+			//Bill b = s.getBill(selectedBill);
+			/*for(Order o2 : Controller.listAllCurrentOrders()) {
 				currentOrders.put(index, o2);
 				currentOrdersComboBox.addItem("Order # " + o2.getNumber());
 				index++;
-			};
-			
-			index = 0;
-			for(Table t2 : o.getTables()) {
-				tables.put(index, t2);
-				tablesComboBox.addItem("Table # " + t2.getNumber());
-				index++;
-			};
-			
-			index = 0;
+			};*/
+		
+			//index = 0;
+			/*index = 0;
 			for(Seat s2 : t.getCurrentSeats()) {
-				currentSeats.put(index, s2);
+				seats.put(index, s2);
 				currentSeatsComboBox.addItem("Seat # " + t.indexOfCurrentSeat(s2));
 				index++;
 			}
-			
+
+			index = 0;
 			for(Bill b : ra.getBills()) {
 				bills.put(index, b);
 				billsComboBox.addItem("Bill # " + ra.indexOfBill(b));
-			}
+				index++;
+			}*/
 		}
 		pack();
 		
 	}
 	
+	private void addSeatToListButtonActionPerformed(java.awt.event.ActionEvent evt) {
+		if(!currentSeats.contains(seats.get(selectedSeat))){
+			currentSeats.add(seats.get(selectedSeat));
+		}
+	}
+	
 	private void issueBillButtonActionPerformed(java.awt.event.ActionEvent evt) {
 		// clear error message and basic input validation
 		error = "";
-		if (selectedOrder < 0 || selectedTable < 0 || selectedSeat < 0) {
+		/*if (selectedOrder < 0 || selectedTable < 0 || selectedSeat < 0) {
 			error = "Must select an order, table, and seat.";
-		}
+		}*/
 
 		if (error.length() == 0) {
 			// call the controller
 			try {
-				Controller.issueNewBill(currentOrders.get(selectedOrder), currentSeats.get(selectedSeat));
+				Controller.issueBill(currentSeats);
 			} catch (InvalidInputException e) {
 				createErrorFrame(e.getMessage());
+				this.dispose();
 			}
 		}
 		refreshData();
 	}
 	
-	private void addToBillButtonActionPerformed(java.awt.event.ActionEvent evt) {
+	private void generateCouponButtonActionPerformed(java.awt.event.ActionEvent evt) {
+		String per = percentageInput.getText(); 
+		double perD = Double.parseDouble(per); 
+		double perF = perD/100; 
+		long retID = 0; 
+		try {
+			retID = Controller.addCoupon(perF);
+		} catch (InvalidInputException e) {
+			createErrorFrame(e.getMessage());
+			this.dispose();
+		} 
+		System.out.println(retID);
+		
+	}
+	
+	/*private void addToBillButtonActionPerformed(java.awt.event.ActionEvent evt) {
 		// clear error message and basic input validation
 		error = "";
 		if (selectedOrder < 0 || selectedTable < 0 || selectedSeat < 0 || selectedBill < 0) {
@@ -260,6 +321,7 @@ public class IssueBillPage extends JFrame{
 
 		if (error.length() == 0) {
 			// call the controller
+			
 			try {
 				Controller.addSeatToBill(bills.get(selectedBill), currentSeats.get(selectedSeat));
 			} catch (InvalidInputException e) {
@@ -267,7 +329,7 @@ public class IssueBillPage extends JFrame{
 			}
 		}
 		refreshData();
-	}
+	}*/
 	
 	private void createErrorFrame(String error){
 		JFrame errorFrame = new JFrame();
@@ -282,6 +344,5 @@ public class IssueBillPage extends JFrame{
 		errorFrame.add(newInfo);
 
 		errorFrame.setVisible(true);
-
 	}
 }
